e2e32bf783e8f72ce92dffc9b765190e2bb861e8
Francode <franco.ojeda.v8@gmail.com>
2023-08-26 08:41:02 -0300

    #1 "Initial commit: Project structure and POM setup

---
+++ src/main/java/com/francode/pricequeryapi/PricequeryapiApplication.java
@@ -0,0 +1,13 @@
+package com.francode.pricequeryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricequeryapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricequeryapiApplication.class, args);
+    }
+
+}
