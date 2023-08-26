75d18696d413393e866052db1555baaa56936dfb
Francode <franco.ojeda.v8@gmail.com>
2023-08-26 12:25:54 -0300

    #12 Adds basic repositories impl

--- src/main/java/com/francode/pricequeryapi/domain/port/ProductRepository.java
+++ src/main/java/com/francode/pricequeryapi/domain/port/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.francode.pricequeryapi.domain.port;
 
+import com.francode.pricequeryapi.domain.model.Product;
+
+import java.util.Optional;
+
 public interface ProductRepository {
+    Optional<Product> findById(Long productId);
 }
